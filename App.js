3656572e6cc232de09a773de320ca4e7e2d451cc
ash-047 <147508168+ash-047@users.noreply.github.com>
2023-11-20 19:28:07 +0530

    Replace the existing App.js file with this one

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Login from './Login';
+import GuidelinesPage from './Guidelines';
+import AboutUs from './About';
+import BorrowedBooks from './BorrowedBooks';
+import Search from './Search';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+};
+
+export default App;
